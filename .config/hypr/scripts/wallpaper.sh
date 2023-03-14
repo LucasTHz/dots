94c9385cec824b072af1b1bff562402a5b51f299
LucasTHz <lucasthomaz58@gmail.com>
2023-03-13 23:56:49 -0300

    add swww

--- .config/hypr/scripts/wallpaper.sh
+++ .config/hypr/scripts/wallpaper.sh
@@ -1,11 +1,32 @@
-#!/bin/sh
-swaybg -i $(find ~/Pictures/Imagens/* -type f | shuf -n1) -m fill &
-OLD_PID=$!
+# !/bin/bash
+
+# This script will randomly go through the files of a directory, setting it
+# up as the wallpaper at regular intervals
+#
+# NOTE: this script is in bash (not posix shell), because the RANDOM variable
+# we use is not defined in posix
+
+# if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
+# 	echo "Usage:
+# 	$0 <dir containg images>"
+# 	exit 1
+# fi
+
+# Edit bellow to control the images transition
+export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION_STEP=5
+
+# This controls (in seconds) when to switch to the next image
+INTERVAL=300
+
 while true; do
-    sleep 3000
-    swaybg -i $(find ~/Pictures/Imagens/* -type f | shuf -n1) -m fill &
-    NEXT_PID=$!
-    sleep 5
-    kill $OLD_PID
-    OLD_PID=$NEXT_PID
+	find ~/Pictures/Imagens/* -type f | shuf -n1 \
+		| while read -r img; do
+			echo "$((RANDOM % 1000)):$img"
+		done \
+		| sort -n | cut -d':' -f2- \
+		| while read -r img; do
+			swww img "$img"
+			sleep $INTERVAL
+		done
 done
